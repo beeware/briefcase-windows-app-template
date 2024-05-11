940735d9f641487c9ff756d22024196c24cbc6d7
Russell Keith-Magee <russell@keith-magee.com>
2024-05-11 11:12:57 +0800

    Modify stub handling to allow for console and GUI stubs.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,9 +4,12 @@ from pathlib import Path
 
 BIN_PATH = Path("src")
 
-# Move the stub for the Python version into the final location
-os.rename(BIN_PATH / 'Stub-{{ cookiecutter.python_version|py_tag }}.exe', BIN_PATH / '{{ cookiecutter.formal_name }}.exe')
+# Rename the stub binary we want
+STUB_PATH = (
+    BIN_PATH / '{% if cookiecutter.console_app %}Console{% else %}GUI{% endif %}-Stub-{{ cookiecutter.python_version|py_tag }}.exe'
+)
+STUB_PATH.rename(BIN_PATH / "Stub.exe")
 
 # Delete all remaining stubs
-for stub in BIN_PATH.glob("Stub-*.exe"):
+for stub in BIN_PATH.glob("*-Stub-*.exe"):
     os.unlink(stub)
