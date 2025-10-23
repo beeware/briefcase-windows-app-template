435f50c103df1f7f2e297fc760f7bb73230ffbb4
Russell Keith-Magee <russell@keith-magee.com>
2025-10-24 06:08:01 +0800

    Remove prefix and debug from pre/post scripts.

--- {{ cookiecutter.format }}/extras/{{ cookiecutter.installer_path }}/run_post_install.bat
+++ {{ cookiecutter.format }}/extras/{{ cookiecutter.installer_path }}/run_post_install.bat
@@ -3,12 +3,7 @@ setlocal enabledelayedexpansion
 SET ALLUSERS=%1
 {% for name in cookiecutter.install_options -%}
 {%- set argn = loop.index + 1 -%}
-SET {{ name.upper() }}=%{{ argn }}
+SET OPTION_{{ name.upper() }}=%{{ argn }}
 {% endfor -%}
 
 CALL "%~dp0post_install.bat"
-
-echo ALLUSER=%ALLUSERS%
-{% for name in cookiecutter.install_options -%}
-echo {{ name }}=%{{ name.upper() }}%
-{% endfor -%}
