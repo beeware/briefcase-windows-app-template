01032fb11dfb2f4941557400e2aa345cdf807586
Russell Keith-Magee <russell@keith-magee.com>
2025-12-02 15:16:42 +0800

    Use OPTION_ prefix for uninstall options.

--- {{ cookiecutter.format }}/extras/{{ cookiecutter.installer_path }}/run_pre_uninstall.bat
+++ {{ cookiecutter.format }}/extras/{{ cookiecutter.installer_path }}/run_pre_uninstall.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal enabledelayedexpansion
 {% for name in cookiecutter.uninstall_options -%}
-SET UNINSTALL_{{ name.upper() }}=%{{ loop.index }}
+SET OPTION_{{ name.upper() }}=%{{ loop.index }}
 {% endfor -%}
 
 CALL "%~dp0pre_uninstall.bat"
