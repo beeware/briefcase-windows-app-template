963d0d61daf11344b656430d45701eab70407f60
Russell Keith-Magee <russell@keith-magee.com>
2025-12-03 06:10:36 +0800

    Add uninstall options to MSI installers (#79)

--- {{ cookiecutter.format }}/extras/{{ cookiecutter.installer_path }}/run_pre_uninstall.bat
+++ {{ cookiecutter.format }}/extras/{{ cookiecutter.installer_path }}/run_pre_uninstall.bat
@@ -1,4 +1,7 @@
 @echo off
 setlocal enabledelayedexpansion
+{% for name in cookiecutter.uninstall_options -%}
+SET OPTION_{{ name.upper() }}=%{{ loop.index }}
+{% endfor -%}
 
 CALL "%~dp0pre_uninstall.bat"
