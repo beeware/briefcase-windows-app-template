3006f692aecec5b8597e260846faf5a1d4fd6499
Russell Keith-Magee <russell@keith-magee.com>
2026-05-30 07:29:30 +0800

    Miscellaneous improvements to MSI installers (#94)

--- {{ cookiecutter.format }}/extras/{{ cookiecutter.installer_path }}/run_post_install.bat
+++ {{ cookiecutter.format }}/extras/{{ cookiecutter.installer_path }}/run_post_install.bat
@@ -2,8 +2,9 @@
 setlocal enabledelayedexpansion
 SET ALLUSERS=%1
 SET INSTALLER_PATH=%2
+SET INSTALLER_UNATTENDED=%3
 {% for name in cookiecutter.install_options -%}
-{%- set argn = loop.index + 2 -%}
+{%- set argn = loop.index + 3 -%}
 SET OPTION_{{ name.upper() }}=%{{ argn }}
 {% endfor -%}
 
--- {{ cookiecutter.format }}/extras/{{ cookiecutter.installer_path }}/run_pre_uninstall.bat
+++ {{ cookiecutter.format }}/extras/{{ cookiecutter.installer_path }}/run_pre_uninstall.bat
@@ -1,7 +1,9 @@
 @echo off
 setlocal enabledelayedexpansion
+SET INSTALLER_UNATTENDED=%1
 {% for name in cookiecutter.uninstall_options -%}
-SET OPTION_{{ name.upper() }}=%{{ loop.index }}
+{%- set argn = loop.index + 1 -%}
+SET OPTION_{{ name.upper() }}=%{{ argn }}
 {% endfor -%}
 
 CALL "%~dp0pre_uninstall.bat"
